a380e35fabe490b37ce17b444b2f4645a6d67c3a
Dylan Ierugan <dylanierugan@gmail.com>
2024-08-23 10:51:25 -0700

    Initial Commit

---
+++ Dessert List/Dessert_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dessert_ListApp.swift
+//  Dessert List
+//
+//  Created by Dylan Ierugan on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Dessert_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
